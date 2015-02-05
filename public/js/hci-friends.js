ac36ff9eff3ca087dfed22f139e1ff5fbf4ea68a
Michael LaPlante <mflaplan@ucsd.edu>
2015-02-05 15:27:29 -0800

    submitting lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -3,6 +3,11 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+	$(".friend-name").click(function(event) {
+		event.preventDefault();
+		$(this).text(anagrammedName($(this).text()));
+	});
 })
 
 /*
